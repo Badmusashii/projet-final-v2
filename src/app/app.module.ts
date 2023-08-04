a03eda722d3af50ae7bdc8afca403b5c6a341033
KILINC Bayram (SNCF VOYAGEURS / DIRECTION DE LIGNE PARIS GRAND EST / EQUIPE TRAIN PARIS EST 4) <8103509m@commun.ad.sncf.fr>
2023-08-04 08:23:51 +0200

    ajout des pages et composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ConnexionComponent } from './pages/connexion/connexion.component';
+import { PlatformsComponent } from './pages/platforms/platforms.component';
+import { CardComponent } from './components/card/card.component';
+import { SearchbarComponent } from './components/searchbar/searchbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ConnexionComponent,
+    PlatformsComponent,
+    CardComponent,
+    SearchbarComponent
   ],
   imports: [
     BrowserModule,
